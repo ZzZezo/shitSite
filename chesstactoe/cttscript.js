73c6e3843574c8a598e748d8616b1e14b32c144d
ZzZezo <98333953+ZzZezo@users.noreply.github.com>
2024-07-02 15:04:37 +0200

    ctt: Librarys + centering

--- chesstactoe/cttscript.js
+++ chesstactoe/cttscript.js
@@ -1,5 +1,5 @@
 function createBoard(size) {
-    const mainBoard = document.getElementById('mainboard');
+    const mainBoard = document.getElementById("mainboard");
 
     for (let row = 0; row < size; row++) {
         const tr = document.createElement('tr');
@@ -20,4 +20,30 @@ function createBoard(size) {
         mainBoard.appendChild(tr);
     }
 }
+
+function createLibrary(size,color){ //was cooked, look what i got meanwhile https://i.imgur.com/iQFudu9.png
+    const libraryBoard = document.getElementById(color+"Library");
+
+    for (let row = 0; row < size; row++) {
+        const tr = document.createElement('tr');
+
+        for (let col = 0; col < 1; col++) {
+            const td = document.createElement('td');
+
+            //check color
+            if ((row + col) % 2 === 0) {
+                td.className = 'white';
+            } else {
+                td.className = 'black';
+            }
+
+            tr.appendChild(td);
+        }
+
+        libraryBoard.appendChild(tr);
+    }
+}
+
 createBoard(3);
+createLibrary(8,"white");
+createLibrary(8,"black");
