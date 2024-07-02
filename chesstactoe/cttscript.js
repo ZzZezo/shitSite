482cd21b5eddddb3ea84eb0f04959aaa61edfaec
ZzZezo <98333953+ZzZezo@users.noreply.github.com>
2024-07-02 15:44:35 +0200

    ctt: pieces + dynamic placing

--- chesstactoe/cttscript.js
+++ chesstactoe/cttscript.js
@@ -13,7 +13,6 @@ function createBoard(size) {
             } else {
                 td.className = 'black';
             }
-
             tr.appendChild(td);
         }
 
@@ -21,8 +20,9 @@ function createBoard(size) {
     }
 }
 
-function createLibrary(size,color){ //was cooked, look what i got meanwhile https://i.imgur.com/iQFudu9.png
+function createLibrary(size,color,figures){ //was cooked, look what i got meanwhile https://i.imgur.com/iQFudu9.png
     const libraryBoard = document.getElementById(color+"Library");
+    let slot = 0;
 
     for (let row = 0; row < size; row++) {
         const tr = document.createElement('tr');
@@ -37,6 +37,9 @@ function createLibrary(size,color){ //was cooked, look what i got meanwhile http
                 td.className = 'black';
             }
 
+            td.innerHTML =figures[slot];
+            slot++;
+
             tr.appendChild(td);
         }
 
@@ -44,6 +47,38 @@ function createLibrary(size,color){ //was cooked, look what i got meanwhile http
     }
 }
 
+
+function drawTextures(board){
+    const mainBoard = document.getElementById(board);
+    const rows = mainBoard.querySelectorAll("tr");
+    rows.forEach(row => {
+        const cells = row.querySelectorAll("td");
+        cells.forEach(cell => {
+            const cellText = cell.textContent.trim();
+            const img = document.createElement("img");
+
+            if(cellText == "bishop") img.src = "assets/images/ctt/bishop.png";
+            if(cellText == "king") img.src = "assets/images/ctt/king.png";
+            if(cellText == "knight") img.src = "assets/images/ctt/knight.png";
+            if(cellText == "pawn") img.src = "assets/images/ctt/pawn.png";
+            if(cellText == "queen") img.src = "assets/images/ctt/queen.png";
+            if(cellText == "rook") img.src = "assets/images/ctt/rook.png";
+            
+            img.style.width = "60px";
+            img.style.height = img.style.width;
+
+            cell.appendChild(img);
+            console.log("???")
+        })
+    })
+}
+
+
+//code that actually runs
 createBoard(3);
-createLibrary(8,"white");
-createLibrary(8,"black");
+createLibrary(8,"white",["rook","knight","bishop","bishop","knight","rook","pawn","pawn"]);
+createLibrary(8,"black",["rook","knight","bishop","bishop","knight","rook","pawn","pawn"]);
+
+drawTextures("mainboard");
+drawTextures("whiteLibrary");
+drawTextures("blackLibrary");
