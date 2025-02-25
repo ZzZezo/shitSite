220813717f24bb7cff06505660a40de50db82a3b
ZzZezo <98333953+ZzZezo@users.noreply.github.com>
2025-02-25 01:29:47 +0100

    nevermind lol that shit was cooked

--- .netlify/functions/getCounter.js
+++
@@ -1,8 +0,0 @@
-const counter = require('./counter.json');  // Assuming you store counter value in a JSON file.
-
-exports.handler = async () => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ counter: counter.value }),
-  };
-};
